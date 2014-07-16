2daa27940f92cbf853d7ee04ac0c42afa5a3381f
wangchangzhong <wangchangzhong@6bb61b2d-3f82-4e89-be87-24d392872a5e>
2014-07-16 02:39:27 +0000

    对 post 相关类的完善主要是 postcontroller 的完善

--- trunk/src/main/java/com/ztyj6/fs/controller/PostController.java
+++ trunk/src/main/java/com/ztyj6/fs/controller/PostController.java
@@ -4,12 +4,13 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.ztyj6.fs.model.Post;
+import com.ztyj6.fs.model.page.DataGrid;
 import com.ztyj6.fs.model.page.Json;
+import com.ztyj6.fs.model.page.PageFilter;
 import com.ztyj6.fs.service.IPostService;
 
 @Controller
@@ -38,6 +39,71 @@ public class PostController extends BaseController {
     	}catch(Exception e){
     		msg = "添加失败";
     		json.setMsg(msg);
+    	}
+    	return json;
+    }
+    @ResponseBody
+    @RequestMapping("/admin/delete")
+    public Json delete(Post post){
+    	Json json = new Json();
+    	String msg = "";
+    	try{
+    		postService.delete(post);
+    		json.setSuccess(true);
+    		msg = "删除成功";
+    		json.setMsg(msg);
+    		json.setSuccess(true);
+    	}catch(Exception e){
+    		msg = "删除失败";
+            json.setSuccess(false);
+            json.setMsg(msg);
+    		e.printStackTrace();
+    	}
+    	return json;
+    }
+    @ResponseBody
+    @RequestMapping("/admin/deleteBatch")
+    public Json deleteBatch(String ids){
+    	Json json = new Json();
+    	String msg = "";
+    	try{
+    	    postService.delete(ids);
+    	    msg = "批量删除成功";
+    	    json.setSuccess(true);
+    	    json.setMsg(msg);
+    	}catch(Exception e){
+    		e.printStackTrace();
+    		msg = "批量删除失败";
+    		json.setSuccess(false);
+    		json.setMsg(msg);
+    	}
+    	return json;
+    }
+    @ResponseBody
+    @RequestMapping("/admin/getByPage")
+    public DataGrid getByPage(PageFilter pageFilter){
+    	 try{
+    		 return postService.getByPage(pageFilter);
+    	 }catch(Exception e){
+    		 e.printStackTrace();
+    		 return null;
+    	 }
+    }
+    @ResponseBody
+    @RequestMapping("/admin/update")
+    public Json update(Post post){
+    	Json json = new Json();
+    	String msg = "";
+    	try{
+    		postService.update(post);
+    		msg = "更新成功";
+    		json.setSuccess(true);
+    		json.setMsg(msg);
+    	}catch(Exception e){
+    		msg = "更新失败";
+    		json.setSuccess(false);
+            json.setMsg(msg);
+    		e.printStackTrace();
     	}
     	return json;
     }
